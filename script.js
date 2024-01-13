85cd9f221dcdc84e9811c636e93d017a7ad71146
kwanzv <kwanzeev@gmail.com>
2024-01-13 10:59:50 +0700

    Gameboard factory function + X and O functionality

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-const gameBoard = () => {
+const createGameBoard = (() => {
   const gameBoard = document.querySelector(".game-board");
+  //Storing the gameboard as an array inside a gameBoard object.
 
-  for (let i = 0; i < 9; i++) {
-    const tile = document.createElement("div");
-    tile.classList.add("tile");
-    gameBoard.appendChild(tile);
+  const board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+
+  // Creating the gameboard with a for Each loop using board.
+  let tileId = 1;
+
+  board.forEach((row) => {
+    row.forEach(() => {
+      //Creating the tiles
+      const tileElement = document.createElement("div");
+      tileElement.classList.add("tile");
+      tileElement.dataset.id = tileId;
+      gameBoard.appendChild(tileElement);
+      tileId++;
+    });
+  });
+
+  return { board, gameBoard };
+})();
+
+function Game() {
+  function Player(name, marker) {
+    this.name = name;
+    this.marker;
+    console.log({ name, marker });
+    return { name, marker };
   }
 
-  return { gameBoard };
-};
+  function handleTileClick(event) {
+    if (activeIndex === 0) {
+      event.currentTarget.innerHTML = player1.marker;
+      activeIndex = 1;
+    } else if (activeIndex === 1) {
+      event.currentTarget.innerHTML = player2.marker;
+      activeIndex = 0;
+    }
+  }
 
-gameBoard();
+  document.querySelectorAll(".tile").forEach((tile) => {
+    tile.addEventListener("click", handleTileClick);
+  });
+
+  let activeIndex = 0;
+  const player1 = Player("John", "X");
+  const player2 = Player("Jane", "O");
+
+  return { player1, player2 };
+}
+
+const game = Game();
