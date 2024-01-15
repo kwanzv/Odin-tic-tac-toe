f48e1b57a65562aa3e492d68722b53ee11dca8bb
kwanzv <kwanzeev@gmail.com>
2024-01-15 18:54:58 +0700

    Game logic extended, stops when winner is found

---
+++ script.mjs
@@ -0,0 +1,94 @@
+const createGameBoard = (() => {
+  const gameBoard = document.querySelector(".game-board");
+  //Storing the gameboard as an array inside a gameBoard object.
+
+  const board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+
+  // Creating the gameboard with a for Each loop using board.
+  let tileId = 0;
+
+  board.forEach((row) => {
+    row.forEach(() => {
+      //Creating the tiles
+      const tileElement = document.createElement("div");
+      tileElement.classList.add("tile");
+      gameBoard.appendChild(tileElement);
+    });
+  });
+
+  return { board, gameBoard };
+})();
+
+function Game() {
+  const tiles = document.querySelectorAll(".tile");
+  let activeIndex = 0;
+  const player1 = new Player("Player", "X");
+  const player2 = new Player("Computer", "O");
+
+  function Player(name, marker) {
+    this.name = name;
+    this.marker;
+    return { name, marker };
+  }
+
+  function handleTileClick(event) {
+    if (activeIndex === 0 && !event.currentTarget.innerHTML) {
+      event.currentTarget.innerHTML = player1.marker;
+      activeIndex = 1;
+    } else if (activeIndex === 1 && !event.currentTarget.innerHTML) {
+      event.currentTarget.innerHTML = player2.marker;
+      activeIndex = 0;
+    }
+    checkWin({ ...tiles });
+  }
+
+  function stopGame() {
+    tiles.forEach((tile) => {
+      tile.removeEventListener("click", handleTileClick);
+    });
+  }
+
+  function checkWin(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a].innerHTML &&
+        squares[a].innerHTML === squares[b].innerHTML &&
+        squares[a].innerHTML === squares[c].innerHTML
+      ) {
+        console.log("Winner is: " + squares[a].innerHTML);
+        return stopGame();
+      }
+    }
+    return null;
+  }
+  tiles.forEach((tile) => {
+    tile.addEventListener("click", handleTileClick);
+  });
+}
+
+// const playerName = document.querySelector(".player-name");
+// const name = document.querySelector("#name");
+
+const beginGame = document
+  .querySelector("#start-game")
+  .addEventListener("click", () => {
+    createGameBoard.gameBoard.style.background = "#f0f0f0";
+    console.log("I'm trying");
+    Game();
+  });
